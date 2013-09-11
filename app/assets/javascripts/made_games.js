fd98d0b9b6f5a59087994331760f7687a396f39a
Kathryn <keb97@cornell.edu>
2013-09-11 15:59:12 -0700

    now the jquery is OK with Rails4 having turbolinks

--- app/assets/javascripts/made_games.js
+++ app/assets/javascripts/made_games.js
@@ -1,71 +1,41 @@
-$(document).ready(function(){
-  $('#made_game_text1').on('keyup', function(){
-    $('#post-preview').html($('#made_game_text1').val());
-  });
-});
+$(document).on('page:load', function(){
+  $(document).ready(function(){
+    $('#made_game_text1').on('keyup', function(){
+      $('#post-preview').html($('#made_game_text1').val());
+    });
+    $('#made_game_text2').on('keyup', function(){
+      $('#post-preview-2').html($('#made_game_text2').val());
+    });
+    $('#made_game_text3').on('keyup', function(){
+      $('#post-preview-3').html($('#made_game_text3').val());
+    });
+    $('#made_game_text4').on('keyup', function(){
+      $('#post-preview-4').html($('#made_game_text4').val());
+    });
+    $('#made_game_text5').on('keyup', function(){
+      $('#post-preview-5').html($('#made_game_text5').val());
+    });
+    $('#made_game_text6').on('keyup', function(){
+      $('#post-preview-6').html($('#made_game_text6').val());
+    });
+    $('#made_game_word1').on('keyup', function(){
+      $('#post-preview-7').html($('#made_game_word1').val());
+    });
+    $('#made_game_word2').on('keyup', function(){
+      $('#post-preview-8').html($('#made_game_word2').val());
+    });
+    $('#made_game_word3').on('keyup', function(){
+      $('#post-preview-9').html($('#made_game_word3').val());
+    });
 
-$(document).ready(function(){
-  $('#made_game_text2').on('keyup', function(){
-    $('#post-preview-2').html($('#made_game_text2').val());
-  });
-});
-
-$(document).ready(function(){
-  $('#made_game_text3').on('keyup', function(){
-    $('#post-preview-3').html($('#made_game_text3').val());
-  });
-});
-
-$(document).ready(function(){
-  $('#made_game_text4').on('keyup', function(){
-    $('#post-preview-4').html($('#made_game_text4').val());
-  });
-});
-
-$(document).ready(function(){
-  $('#made_game_text5').on('keyup', function(){
-    $('#post-preview-5').html($('#made_game_text5').val());
-  });
-});
-
-$(document).ready(function(){
-  $('#made_game_text6').on('keyup', function(){
-    $('#post-preview-6').html($('#made_game_text6').val());
-  });
-});
-
-$(document).ready(function(){
-  $('#made_game_word1').on('keyup', function(){
-    $('#post-preview-7').html($('#made_game_word1').val());
-  });
-});
-
-$(document).ready(function(){
-  $('#made_game_word2').on('keyup', function(){
-    $('#post-preview-8').html($('#made_game_word2').val());
-  });
-});
-
-$(document).ready(function(){
-  $('#made_game_word3').on('keyup', function(){
-    $('#post-preview-9').html($('#made_game_word3').val());
-  });
-});
-
-$(document).ready(function(){
-  $('#made_game_word4').on('keyup', function(){
-    $('#post-preview-10').html($('#made_game_word4').val());
-  });
-});
-
-$(document).ready(function(){
-  $('#made_game_word5').on('keyup', function(){
-    $('#post-preview-11').html($('#made_game_word5').val());
-  });
-});
-
-$(document).ready(function(){
-  $('#made_game_word6').on('keyup', function(){
-    $('#post-preview-12').html($('#made_game_word6').val());
+    $('#made_game_word4').on('keyup', function(){
+      $('#post-preview-10').html($('#made_game_word4').val());
+    });
+    $('#made_game_word5').on('keyup', function(){
+      $('#post-preview-11').html($('#made_game_word5').val());
+    });
+    $('#made_game_word6').on('keyup', function(){
+      $('#post-preview-12').html($('#made_game_word6').val());
+    });
   });
 });
